90aad7feac96981f79361ffa34c405fe8e29824d
sinndu <sindusaram@gmail.com>
2024-02-10 14:30:53 -0500

    build

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function App() {
         </div>
   
         <div class="rightDivM" id="rightDiv">
-            <text className='title'>DILLIAD3</text>
+            <text className='title'>DILLIAD</text>
             <text className='title'>INTEREST FORM</text>
             <div className="padding"></div>
 
